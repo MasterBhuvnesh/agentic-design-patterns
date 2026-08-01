467b9df9a279cac7902cd7f864c5e0ea65fed125
bhuvii <bhuvneshverma2904@gmail.com>
2026-08-02 01:51:24 +0530

    feat : license

--- code/resources/helper.py
+++ code/resources/helper.py
@@ -1,8 +1,11 @@
 from colorama import Fore, Style, init
+from rich.console import Console
+from rich.table import Table
 
+console = Console()
 init(autoreset=True)
 
-def show_response(response, show_reasoning=True, show_usage=True):
+def show_response(response, show_reasoning=False, show_usage=True):
     print(Fore.CYAN + Style.BRIGHT + "\n RESPONSE " + Style.RESET_ALL)
     print(Fore.CYAN + "-" * 40)
     print(response.content)
@@ -13,12 +16,15 @@ def show_response(response, show_reasoning=True, show_usage=True):
         print(Fore.MAGENTA + "-" * 40)
         print(reasoning)
 
-    if show_usage and (usage := response.usage_metadata):
-        print(Fore.GREEN + Style.BRIGHT + "TOKENS " + Style.RESET_ALL)
-        print(Fore.GREEN + "-" * 40)
-        print(
-            f"Input: {usage['input_tokens']} | "
-            f"Output: {usage['output_tokens']} | "
-            f"Total: {usage['total_tokens']}"
-            ,end="\n"
-        )
+    if show_usage and (usage:= response.usage_metadata):
+        print("\n")
+        table = Table(title="TOKEN USAGE", title_style="bold green")
+
+        table.add_column("METRIC", style="cyan")
+        table.add_column("VALUE", justify="right", style="green")
+
+        table.add_row("INPUT", str(usage["input_tokens"]))
+        table.add_row("OUTPUT", str(usage["output_tokens"]))
+        table.add_row("TOTAL", str(usage["total_tokens"]))
+
+        console.print(table)
